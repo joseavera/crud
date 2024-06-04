33ea93731c8b5d6fd6590c39dbf16122acdcf201
@joseavera <69088516+joseavera@users.noreply.github.com>
2024-06-04 09:37:32 -0600

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -6,7 +6,7 @@ $base_datos = "db";
 
 // Crear conexión
 $conn = new mysqli($servidor, $usuario, $password, $base_datos);
-
+//verificacion1
 // Verificar conexión
 /*if ($conn->connect_error) {
     die("Conexión fallida - ERROR de conexión: " . $conn->connect_error);
